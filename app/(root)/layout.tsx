f5df13fae0e8f56332a016ee57d2075b10197940
Aditya <codecommander03@gmail.com>
2024-06-07 23:23:36 +0530

    changed app page hierarchy

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,27 @@
+import SiderBar from "@/components/SiderBar";
+import Image from "next/image";
+
 export default function Layout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const loggedIn = { firstName: "Aditya", lastName: "Gupta" }; 
     return (
-        <main>
-            {/* <Sidebar /> */}
+        <main className="flex h-screen w-full font-inter">
+            <SiderBar user={loggedIn} />
+
+            <div className="flex size-full flex-col">
+                <div className="root-layout">
+                    <Image
+                        src="/icons/logo.svg"
+                        width={30}
+                        height={30}
+                        alt="menu icon"
+                    />
+                    <div></div>
+                </div>
+            </div>
             {children}
         </main>
     );
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
+    const loggedIn = {firstName: 'Aditya'}
+    return (
+        <section className='home'>
+            <div className='home-content'>
+                <header className='home-header'>
+                    <HeaderBox
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn?.firstName || 'Guest'}
+                        subtext="Access and manage your account and transactions efficiently."
+                    />
+
+                    <TotalBalanceBox
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={1234.56}
+                    />
+                </header>
+            </div>
+        </section>
+    )
 }
 
 export default page

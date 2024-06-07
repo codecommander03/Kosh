182c5cc7eb61bfd95a06019e44bb3b5db8f48c44
Aditya <codecommander03@gmail.com>
2024-06-07 23:23:14 +0530

    animated balance

---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,17 @@
+"use client"
+import React from 'react'
+import CountUp from 'react-countup'
+
+const AnimatedCounter = ({amount} : {amount : number}) => {
+    return (
+        <CountUp className='w-full'
+            duration={2.75}
+            decimals={2}
+            decimal=','
+            prefix='$'
+            end={amount}
+        />
+    )
+}
+
+export default AnimatedCounter
